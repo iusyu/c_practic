99902becdae213074a09535afd420257425cf8cb
iuAmy <1195594964@qq.com>
2021-01-28 14:00:16 +0800

    p6 就是测试opendir等函数 打开文件目录，现在的问题是怎么获取目录下文件名对应文件的属性

---
+++ linux_learn/p6.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<sys/types.h>
+#include<unistd.h>
+#include<dirent.h>
+
+
+int main(int argc, char *argv[])
+{
+	//sleep(5);
+	
+	//printf("-t is a time.\n-m is mess.\n-g is game over.\n-p is paosex.\n");
+	//char tmp = 0;
+	
+	
+	struct dirent* dirp;
+	DIR* dird;
+	if( (dird = opendir(argv[1]))== NULL){
+		perror("opendir is fiall !");
+		exit(EXIT_FAILURE);
+	}
+
+	while( (dirp = readdir(dird)) != NULL){
+		printf("files : > %s \n", dirp->d_name);
+	}
+
+	closedir(dird);
+	
+
+
+
+	printf("this is new loading into the memory to executive!\n");
+	printf("parents process ID : %d \n", getpid());
+
+	printf("parents process ID : %d \n", getppid());
+	return 0;
+}
+
+
+
+
