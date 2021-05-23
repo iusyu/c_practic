a091e5414f19ff9b911c8550154f9c095fec3a90
iusk <iuamyy@163.com>
2021-05-23 20:24:36 +0800

    c16 16.2.1 16.36

---
+++ cppprimer_anwser/16.34.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template<class T> int compare( const T& a, const T& b) {
+	std::cout<< a <<std::endl;
+}
+
+
+int main( int argc, char* argv[] ){ 
+	compare("HI","WORLD");
+	compare("BYE", "BYE");
+	return 0;
+}
